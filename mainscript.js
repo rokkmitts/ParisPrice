cab0dd1d667b95da72261e27f86bcef44d433535
RokkitMittens <jtc.copeland@outlook.com>
2024-01-25 03:33:39 -0500

    Create variables for the DOM elements. Build a formula to determine a completed circles radian in relation to the total number of img and video items used. Iterate over each array, create and assign appropriate media element with a source.

--- mainscript.js
+++ mainscript.js
@@ -1,12 +1,14 @@
 "use strict";
 
-const items = document.querySelectorAll(".item");
+const imgItems = document.querySelectorAll(".img-item");
+const vidItems = document.querySelectorAll(".vid-item");
 const container = document.querySelector(".container");
-const numberOfItems = items.length;
+const numberOfImgItems = imgItems.length;
+const numberOfVidItems = vidItems.length;
 
 // ratio of a circle circumference against its diamter.
 // Degree of angle for each img in the container
-const angleIncremeent = (2 * Math.PI) / numberOfItems;
+const angleIncremeent = (2 * Math.PI) / (numberOfImgItems + numberOfVidItems);
 const radius = 300;
 let isGalleryOpen = false;
 
@@ -15,3 +17,13 @@ const centerX = container.offsetWidth / 2;
 const centerY = container.offsetHeight / 2;
 
 const tl = gsap.timeline();
+
+items.forEach(function (item, index) {
+  const img = document.createElement("img");
+  const video = document.createElement("video");
+
+  img.src = "./mediaAssets/img" + (index + 1) + ".jpg";
+  img.appendChild(img);
+
+  video.src = "./mediaAssets/vid" + (index + 1) + ".mp4";
+});
