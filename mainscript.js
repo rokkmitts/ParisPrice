1f5345fc26cb5ee7a2eea3c70af1d98d4a461bb3
RokkitMittens <jtc.copeland@outlook.com>
2024-01-29 03:06:24 -0500

    create variables and tweens/timeline fo positioning each element

--- mainscript.js
+++ mainscript.js
@@ -1,29 +1,42 @@
 "use strict";
 
-const imgItems = document.querySelectorAll(".img-item");
-const vidItems = document.querySelectorAll(".vid-item");
+// // arrays for selected items
+const items = document.querySelectorAll(".item");
 const container = document.querySelector(".container");
-const numberOfImgItems = imgItems.length;
-const numberOfVidItems = vidItems.length;
-
-// ratio of a circle circumference against its diamter.
-// Degree of angle for each img in the container
-const angleIncremeent = (2 * Math.PI) / (numberOfImgItems + numberOfVidItems);
+const numberOfItems = items.length;
+const angleIncremeent = (2 * Math.PI) / numberOfItems;
 const radius = 300;
-let isGalleryOpen = false;
 
-// pixels corresponding to integer
+// condition for click events
+let isGalleryOpen = false;
+// pixel measurement of the container x and y axis
 const centerX = container.offsetWidth / 2;
 const centerY = container.offsetHeight / 2;
 
+// animation sequence shorthand
 const tl = gsap.timeline();
 
 items.forEach(function (item, index) {
   const img = document.createElement("img");
-  const video = document.createElement("video");
+  img.src = "./mediaAssets/img/" + "img" + (index + 1) + ".jpg";
+  item.appendChild(img);
 
-  img.src = "./mediaAssets/img" + (index + 1) + ".jpg";
-  img.appendChild(img);
-
-  video.src = "./mediaAssets/vid" + (index + 1) + ".mp4";
+  const angle = index * angleIncremeent;
+  const initialRotation = (angle * 180) / Math.PI - 90;
+  const x = centerX + radius * Math.cos(angle);
+  const y = centerY + radius * Math.sin(angle);
+  gsap.set(item, { scale: 0 });
+  tl.to(
+    item,
+    {
+      left: x + "px",
+      top: y + "px",
+      rotation: initialRotation,
+      scale: 1,
+      duration: 1,
+      ease: "power2.out",
+      delay: 1,
+    },
+    index * 0.1
+  );
 });
